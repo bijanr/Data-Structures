5b7012fa53ed1a6f0e351011aacce75f99b82b5e
BIJAN ROY <bijanr2004@gmail.com>
2023-10-07 13:25:27 +0530

    dll

--- Basic Assessment/Linked List/doubly_linkedlist.c
+++ Basic Assessment/Linked List/doubly_linkedlist.c
@@ -3,31 +3,21 @@
 
 typedef struct node{
     int data;
+    struct node* prev;
     struct node* next;
-    struct node* previous;
 }Node;
 
+Node* addtoEmpty(Node* head, int data);
 int main(){
-    Node* head = (Node*)calloc(1, sizeof(Node));
-    int limit;
-    Node* list;
-    head = list;
-    list->previous = head;
-    printf("Enter the limit: ");
-    scanf("%d", &limit);
-    printf("Enter the elements: \n");
-    for(int i = 0; i<limit; i++)
-    {
-        Node* temp = (Node*)calloc(1, sizeof(Node));
-        scanf("%d", &temp->data);
-        list->next = (Node*)calloc(1, sizeof(Node));
-        list->next = temp;
-        list = list->next;
-    }
-    list = head->next;
-    while(list != NULL){
-        printf("%d ",list->data);
-        list = list->next;
-    }
-    return 0;
+
+}
+
+Node* addtoEmpty(Node* head, int data){
+    Node* temp = malloc(sizeof(Node));
+    temp->prev = NULL;
+    temp->data = data;
+    temp->next = NULL;
+    head = temp;
+    return head;
+
 }
