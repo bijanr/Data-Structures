bb6bfea6d742e37e0d69d7c1c2f9640c5df9b090
Bijan Roy <bijanr2004@gmail.com>
2024-03-27 12:34:38 +0530

    binary search added

--- Basic Assessment/Sem 4/binary_search.c
+++ Basic Assessment/Sem 4/binary_search.c
@@ -29,6 +29,7 @@ int main(){
     scanf("%d", &key);
     sort(list, limit);
     printf("Want to display the list: \n");
+    scanf("%d", )
     display(list, limit);
     int keySearch = search(list, key, limit);   //storing the status of finding the element inside the list
     (keySearch == -1)? printf("Element not found.\n") : printf("ELement %d is located at %d", key, keySearch+1);
--- Basic Assessment/Sem 4/selection.c
+++ Basic Assessment/Sem 4/selection.c
@@ -1,21 +1,20 @@
-// 1. Start
-// 2. input(list)
-// 3. for(i = 1 to n - 1; step by 1)
-//     min = i
-//     for(j = i+1 to n; step by 1)
-//         if(list[j] < list[min])
-//             min = j
-//     if(min != i)
-//         swap(list[min], list[i])
-// 4. Stop
+/*1. Start
+2. input(list)
+3. for(i = 1 to n - 1; step by 1)
+    min = i
+    for(j = i+1 to n; step by 1)
+        if(list[j] < list[min])
+            min = j
+    if(min != i)
+        swap(list[min], list[i])
+4. Stop */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
 void input(int *list, int limit);
-void sort(int *list, int limit);
-void swap(int *a, int *b);
+
 void display(int *list, int limit);
 
 int main(){
@@ -35,24 +34,7 @@ void input(int *list, int limit){
         scanf("%d", &(list[i]));
 }
 
-void swap(int *a, int *b){
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
 
-void sort(int *list, int limit){
-    int min;
-    for(int i = 0; i < limit - 1; i++)  {
-        min = i;
-        for(int j = i+1; j < limit; j++){
-            if(list[j] < list[min])
-                min = j;
-        }
-        if(min != i)
-            swap(&list[min], &list[i]);
-    }
-}
 
 void display(int *list, int limit)   {
     printf("The sorted array is: \n");
