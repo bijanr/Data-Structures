1eedde4ab53bdfd7a422042de391915d62cf0461
Bijan Roy <bijanr2004@gmail.com>
2024-05-21 17:33:48 +0530

    merge 2 arrays committed

--- Basic Assessment/Sem 4/merge_array.c
+++ Basic Assessment/Sem 4/merge_array.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int merge(int *listA, int* listB, int sizeA, int sizeB, int* result);
-
+void input(int *list, int size);
+void merge(int *listA, int* listB, int sizeA, int sizeB, int* result);
+void display(int *result, int size);
 int main (){
     int *listA, *listB, *result, sizeA, sizeB;
     printf("Enter the size of list A: ");
@@ -10,10 +11,42 @@ int main (){
     printf("Enter the size of list B: ");
     scanf("%d", &sizeB);
     listA = (int*)calloc(sizeA, sizeof(int));
+    printf("Enter list A: \n");
+    input(listA, sizeA);
     listB = (int*)calloc(sizeB, sizeof(int));
+    printf("Enter list B: \n");
+    input(listB, sizeB);
     result = (int*)calloc(sizeA + sizeB, sizeof(int));
     merge(listA, listB, sizeA, sizeB, result);
-    
-
+    display(result, sizeA+sizeB);
     return 0;
+}
+
+
+void input(int *list, int size){
+    for(int i = 0; i < size; i++)
+        scanf("%d", list + i);
+}
+
+void merge(int *listA, int* listB, int sizeA, int sizeB, int* result){
+    int i = 0, j = 0, k = 0;
+    while(i < sizeA && j < sizeB){
+        if(listA[i] <= listB[j])
+            result[k++] = listA[i++];
+        else
+            result[k++] = listB[j++];
+    }
+    while(i < sizeA){
+        result[k++] = listA[i++];
+    }
+    while(j < sizeB){
+        result[k++] = listB[j++];
+    }
+}
+
+void display(int *result, int size){
+    printf("The merged array is: \n");
+    for(int i = 0; i < size; i++)
+        printf("%d ", *(result + i));
+    printf("\n");
 }
--- Basic Assessment/Sorting Algorithms/merge_sort.c
+++ Basic Assessment/Sorting Algorithms/merge_sort.c
@@ -1,86 +1,66 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Merge two subarrays of arr[]
-// First subarray is arr[l..m]
-// Second subarray is arr[m+1..r]
-void merge(int arr[], int l, int m, int r) {
-    int i, j, k;
-    int n1 = m - l + 1;
-    int n2 = r - m;
+void input(int* list, int size);
+void mergeSort(int* list, int left, int right);
+void merge(int* list, int left, int middle, int right);
+void display(int* list, int size);
 
-    /* create temporary arrays */
-    int L[n1], R[n2];
+int main(){
+    int* list, size;
+    printf("Enter the size of list: ");
+    scanf("%d", &size);
+    list = calloc(size, sizeof(int));
+    input(list, size);
+    mergeSort(list, 0, size - 1);
+    display(list, size);
 
-    /* Copy data to temporary arrays L[] and R[] */
-    for (i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[m + 1 + j];
+}
 
-    /* Merge the temporary arrays back into arr[l..r]*/
-    i = 0; // Initial index of first subarray
-    j = 0; // Initial index of second subarray
-    k = l; // Initial index of merged subarray
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        } else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
+void input(int* list, int size){
+    printf("Enter the list: ");
+    for(int i = 0; i < size; i++)
+        scanf("%d", list + i);
+}
 
-    /* Copy the remaining elements of L[], if there are any */
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-
-    /* Copy the remaining elements of R[], if there are any */
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
+void mergeSort(int* list, int left, int right){
+    if(left < right){
+        int middle = (left + right) / 2;
+        mergeSort(list, left, middle);
+        mergeSort(list, middle + 1, right);
+        merge(list, left, middle, right);
     }
 }
 
-// l is for left index and r is right index of the sub-array of arr to be sorted
-void mergeSort(int arr[], int l, int r) {
-    if (l < r) {
-        // Same as (l+r)/2, but avoids overflow for large l and h
-        int m = l + (r - l) / 2;
-
-        // Sort first and second halves
-        mergeSort(arr, l, m);
-        mergeSort(arr, m + 1, r);
-
-        // Merge the sorted halves
-        merge(arr, l, m, r);
+void merge(int* list, int left, int middle, int right){
+    int i = 0, j = 0, k = 0;
+    int sizeA = middle - left + 1;
+    int sizeB = right - middle;
+    //allocating two new lists for merging
+    int *listA = calloc(sizeA, sizeof(int));
+    int *listB = calloc(sizeB, sizeof(int));
+    //filling up the two lists with existing values from list
+    for(int i = 0; i < sizeA; i++)
+        *(listA + i) = *(list + (left + i));
+    for(int j = 0; j < sizeB; j++)
+        *(listB + j) = *(list + (middle + 1 + j));
+    i = 0, j = 0, k = left;
+    //comparing and merging sorted arrays
+    while(i < sizeA && j < sizeB){
+        if(*(listA + i) < *(listB + j))
+            list[k++] = listA[i++];
+        else
+            list[k++] = listB[j++];
     }
+    //remaining elements filled uo
+    while(i < sizeA)
+        list[k++] = listA[i++];
+    while(j < sizeB)
+        list[k++] = listB[j++];
 }
 
-// Function to print an array
-void printArray(int A[], int size) {
-    int i;
-    for (i = 0; i < size; i++)
-        printf("%d ", A[i]);
-    printf("\n");
-}
-
-// Driver program to test above functions
-int main() {
-    int arr[] = {12, 11, 13, 5, 6, 7};
-    int arr_size = sizeof(arr) / sizeof(arr[0]);
-
-    printf("Given array is \n");
-    printArray(arr, arr_size);
-
-    mergeSort(arr, 0, arr_size - 1);
-
-    printf("\nSorted array is \n");
-    printArray(arr, arr_size);
-    return 0;
+void display(int* list, int size){
+    printf("The sorted list is: ");
+    for(int i = 0; i < size; i++)
+        printf("%d ", *(list + i));
 }
